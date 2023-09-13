e92778cc6f70d80ea2dcc6fde3b79a089072a86e
tet1917 <tetsu0224.soccer@icloud.com>
2023-09-13 14:26:40 +0900

    test

---
+++ redux-counter-app-tutorial/src/App.js
@@ -0,0 +1,22 @@
+import { useDispatch, useSelector } from 'react-redux';
+import './App.css';
+import { decrement, increment, incrementByAmount } from './redux/counterSlice';
+import { useState } from 'react';
+
+function App() {
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
+
+  const [incrementAmount , setIncrementAmount] = useState("2");
+  return (
+    <div className="App">
+      <h1>count:{count}</h1>
+      <input onChange={(e) => setIncrementAmount(e.target.value)} value={incrementAmount} />
+      <button onClick={() => dispatch(increment())}>➕</button>
+      <button onClick={() => dispatch(decrement())}>➖</button>
+      <button onClick={() => dispatch(incrementByAmount(Number(incrementAmount)))}>追加</button>
+    </div>
+  );
+}
+
+export default App;
